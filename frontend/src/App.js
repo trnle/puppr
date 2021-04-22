c88a86ccabe1d563a301b73a6c0f4b62cc97386f
Tran <tranle97@gmail.com>
2021-04-22 17:09:18 -0500

    Set up skeleton

---
+++ frontend/src/App.js
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from 'react';
+import { useDispatch } from "react-redux";
+import { Route, Switch } from 'react-router-dom';
+import LoginFormPage from './components/LoginFormPage';
+import SignUpFormPage from './components/SignUpFormPage';
+import Navigation from './components/Navigation';
+import * as sessionActions from "./store/session";
+
+function App() {
+  const dispatch = useDispatch();
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    dispatch(sessionActions.restoreUser()).then(() => setIsLoaded(true));
+  }, [dispatch]);
+
+  return (
+    <>
+      <Navigation isLoaded={isLoaded} />
+      {isLoaded && (
+        <Switch>
+          <Route path="/login">
+            <LoginFormPage />
+          </Route>
+          <Route path="/signup">
+            <SignUpFormPage />
+          </Route>
+        </Switch>
+      )}
+    </>
+  );
+}
+
+export default App;
