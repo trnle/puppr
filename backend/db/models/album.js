9ac4f946bf9a794e494bd5f9ee76bacf78279048
Tran <tranle97@gmail.com>
2021-04-26 17:46:43 -0500

    Typo on models

--- backend/db/models/album.js
+++ backend/db/models/album.js
@@ -21,7 +21,7 @@ module.exports = (sequelize, DataTypes) => {
       otherKey: 'photoId',
       through: 'AlbumPhoto'
     }
-    Album.belongsToMany(model.Photo, columnMapping);
+    Album.belongsToMany(models.Photo, columnMapping);
   };
   return Album;
 };
