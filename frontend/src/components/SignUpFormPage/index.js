ebd19a233701cd7f8cb41911c24636893b6caefb
Tran <tranle97@gmail.com>
2021-04-25 21:00:07 -0500

    Floating label

--- frontend/src/components/SignUpFormPage/index.js
+++ frontend/src/components/SignUpFormPage/index.js
@@ -16,12 +16,11 @@ function SignUpFormPage() {
   const [errors, setErrors] = useState([]);
 
   if (sessionUser) return <Redirect to="/" />;
-
   const handleSubmit = (e) => {
     e.preventDefault();
     if (password === confirmPassword) {
       setErrors([]);
-      return dispatch(sessionActions.signup({ firstName, lastName, password, email, username}))
+      return dispatch(sessionActions.signup({ firstName, lastName, password, email, username }))
         .catch(async (res) => {
           const data = await res.json();
           if (data && data.errors) setErrors(data.errors);
@@ -30,66 +29,115 @@ function SignUpFormPage() {
     return setErrors(['Confirm Password field must be the same as the Password field']);
   };
 
+  const FloatLabel = (() => {
+    // add active class
+    const handleFocus = (e) => {
+      const target = e.target;
+      target.parentNode.classList.add('active');
+    };
+
+    // remove active class
+    const handleBlur = (e) => {
+      const target = e.target;
+      if (!target.value) {
+        target.parentNode.classList.remove('active');
+      }
+      target.removeAttribute('placeholder');
+    };
+
+    // register events
+    const bindEvents = (element) => {
+      const floatField = element.querySelector('input');
+      floatField.addEventListener('focus', handleFocus);
+      floatField.addEventListener('blur', handleBlur);
+    };
+
+    // get DOM elements
+    const init = () => {
+      const floatContainers = document.querySelectorAll('.float-container');
+      floatContainers.forEach((element) => {
+        if (element.querySelector('input').value) {
+          element.classList.add('active');
+        }
+        bindEvents(element);
+      });
+    };
+
+    return {
+      init: init
+    };
+  })();
+
+  FloatLabel.init();
+
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit} className='signup-container'>
       <ul>
         {errors.map((error, idx) => <li key={idx}>{error}</li>)}
       </ul>
-      <label>
-        First Name
-        <input
-          type="text"
-          value={firstName}
-          onChange={(e) => setFirstName(e.target.value)}
-          required
-        />
-      </label>
-      <label>
-        Last Name
-        <input
-          type="text"
-          value={lastName}
-          onChange={(e) => setLastName(e.target.value)}
-          required
-        />
-      </label>
-      <label>
-        Username
-        <input
-          type="text"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          required
-        />
-      </label>
-      <label>
-        Email
-        <input
-          type="text"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-        />
-      </label>
-      <label>
-        Password
-        <input
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-      </label>
-      <label>
-        Confirm Password
-        <input
-          type="password"
-          value={confirmPassword}
-          onChange={(e) => setConfirmPassword(e.target.value)}
-          required
-        />
-      </label>
-      <button type="submit">Sign Up</button>
+      <div className='signup-form'>
+        <div id='floatContainer' className='float-container'>
+          <label>First Name</label>
+          <input
+            id='floatField'
+            type='text'
+            value={firstName}
+            onChange={(e) => setFirstName(e.target.value)}
+            required
+          />
+        </div>
+        <div id='floatContainer' className='float-container'>
+          <label>Last Name</label>
+          <input
+            id='floatField'
+            type='text'
+            value={lastName}
+            onChange={(e) => setLastName(e.target.value)}
+            required
+          />
+        </div>
+        <div id='floatContainer' className='float-container'>
+          <label>Username</label>
+          <input
+            id='floatField'
+            type='text'
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
+        </div>
+        <div id='floatContainer' className='float-container'>
+          <label>Email</label>
+          <input
+            id='floatField'
+            type='text'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
+        <div id='floatContainer' className='float-container'>
+          <label>Password</label>
+          <input
+            id='floatField'
+            type='password'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <div id='floatContainer' className='float-container'>
+          <label>Confirm Password</label>
+          <input
+            id='floatField'
+            type='password'
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+            required
+          />
+        </div>
+        <button type='submit'>Sign Up</button>
+      </div>
     </form>
   );
 }
