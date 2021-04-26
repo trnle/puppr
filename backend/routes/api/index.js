36dcbbef6b8ea07bf106186c5f10df130b72492d
Tran <tranle97@gmail.com>
2021-04-26 12:58:04 -0500

    Navigate splash and explore pages

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,9 +1,12 @@
 const router = require('express').Router();
 const sessionRouter = require('./session.js');
 const usersRouter = require('./users.js');
+const exploreRouter = require('./explore.js');
 
 router.use('/session', sessionRouter);
 
 router.use('/users', usersRouter);
+
+router.use('/explore', exploreRouter);
 
 module.exports = router;
