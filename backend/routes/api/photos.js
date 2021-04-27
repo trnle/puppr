f75776fba77ffb4efc70d73f843d8e6990f6c118
Tran <tranle97@gmail.com>
2021-04-26 23:47:35 -0500

    start backend for photos

--- backend/routes/api/photos.js
+++ backend/routes/api/photos.js
@@ -4,4 +4,19 @@ const asyncHandler = require('express-async-handler');
 const { setTokenCookie, requireAuth } = require('../../utils/auth');
 const { Photo } = require('../../db/models');
 
+const { singlePublicFileUpload, singleMulterUpload } = require('../../aswS3');
+
 const router = express.Router();
+
+router.get('/:id(\\d+)', singleMulterUpload('image'), asyncHandler(async (req, res, next) => {
+  const { photoId } = req.params;
+  let photo = await Photo.findByPk(photoId);
+  return res.json({ photo });
+}))
+
+// router.get('/', requireAuth, singleMulterUpload('image'), asyncHandler(async (req, res, next) => {
+//   const { user } = req;
+//   const { title } = req.body;
+// }))
+
+module.exports = router;
