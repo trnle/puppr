ceb91a3b8244b9b065403f5ca4985a1cab69c3cf
Tran <tranle97@gmail.com>
2021-04-27 11:02:00 -0500

    update authorized access

--- backend/routes/api/photos.js
+++ backend/routes/api/photos.js
@@ -1,22 +1,22 @@
-const express = require('express')
-const asyncHandler = require('express-async-handler');
+// const express = require('express')
+// const asyncHandler = require('express-async-handler');
 
-const { setTokenCookie, requireAuth } = require('../../utils/auth');
-const { Photo } = require('../../db/models');
+// const { setTokenCookie, requireAuth } = require('../../utils/auth');
+// const { Photo } = require('../../db/models');
 
-const { singlePublicFileUpload, singleMulterUpload } = require('../../aswS3');
+// const { singlePublicFileUpload, singleMulterUpload } = require('../../awsS3');
 
-const router = express.Router();
+// const router = express.Router();
 
-router.get('/:id(\\d+)', singleMulterUpload('image'), asyncHandler(async (req, res, next) => {
-  const { photoId } = req.params;
-  let photo = await Photo.findByPk(photoId);
-  return res.json({ photo });
-}))
-
-// router.get('/', requireAuth, singleMulterUpload('image'), asyncHandler(async (req, res, next) => {
-//   const { user } = req;
-//   const { title } = req.body;
+// router.get('/:id(\\d+)', singleMulterUpload('image'), asyncHandler(async (req, res, next) => {
+//   const { photoId } = req.params;
+//   let photo = await Photo.findByPk(photoId);
+//   return res.json({ photo });
 // }))
 
-module.exports = router;
+// // router.get('/', requireAuth, singleMulterUpload('image'), asyncHandler(async (req, res, next) => {
+// //   const { user } = req;
+// //   const { title } = req.body;
+// // }))
+
+// module.exports = router;
--- frontend/src/components/SplashPage/index.js
+++ frontend/src/components/SplashPage/index.js
@@ -1,22 +1,30 @@
 // import React, { useState, useEffect } from "react";
-// import { useSelector, useDispatch } from 'react-redux'
-// import { Redirect, useHistory } from 'react-router-dom';
+import { useSelector } from 'react-redux'
+import { Redirect } from 'react-router-dom';
 import DemoButton from '../DemoButton';
 import './SplashPage.css'
 
 
 function SplashPage() {
-  return (
-    <div className='splash-page'>
-      <div className='intro-text'>
-        <h1>Find your happiness.</h1>
-        <h2>Join the Puppr community, home to tens of billions of photos of dogs.</h2>
-        <div className='demo-users'>
-          <DemoButton />
+  const sessionUser = useSelector(state => state.session.user);
+
+  if (!sessionUser) {
+    return (
+      <div className='splash-page'>
+        <div className='intro-text'>
+          <h1>Find your happiness.</h1>
+          <h2>Join the Puppr community, home to tens of billions of photos of dogs.</h2>
+          <div className='demo-users'>
+            <DemoButton />
+          </div>
         </div>
       </div>
-    </div>
-  )
+    )
+  } else {
+    return (
+      <Redirect to='/explore' />
+    )
+  }
 }
 
 export default SplashPage;
