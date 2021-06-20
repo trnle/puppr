1698585020efb2a788bfcf8b7b8e83b8de84972c
Tran <tranle97@gmail.com>
2021-06-20 18:00:09 -0500

    Add by line

--- frontend/src/components/Footer/index.js
+++ frontend/src/components/Footer/index.js
@@ -3,6 +3,10 @@ import './Footer.css';
 function Footer() {
   return (
     <footer className='footer-container'>
+      <div id='by-line'>
+        Developed by Tran Le
+        <span role='img' aria-label='bee'> &#128029;</span>
+      </div>
       <div className='links'>
         <a href='https://github.com/trnle/'>
           <i className="fab fa-github"></i>
