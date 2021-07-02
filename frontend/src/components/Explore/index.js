9902b9952c225916574b68c1cd37edee3e36bdae
Tran <tranle97@gmail.com>
2021-07-02 17:21:00 -0500

    Make photo page more responsive to mobile

--- frontend/src/components/Explore/index.js
+++ frontend/src/components/Explore/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { Redirect, useHistory } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { getPhotos } from '../../store/photos';
